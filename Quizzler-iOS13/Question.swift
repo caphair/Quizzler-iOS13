916a6f151aaf78dd0ca682a9da2f62842ae04c6a
Christine Phair <cacollins258@gmail.com>
2020-09-16 12:17:28 -0700

    backup file for quizzer app

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Christine Phair on 9/14/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
